79989d4ed5a259ca26cd46feef402bcc1aabd5ea
Alexander-Mandzhiev <meshochek92@gmail.com>
2024-07-22 13:19:16 +0300

    Third commit - end

--- internal/app/store/repository.go
+++ internal/app/store/repository.go
@@ -5,5 +5,6 @@ import "github.com/Alexander-Mandzhiev/http-rest-api-golang/internal/app/model"
 // UserRepository ...
 type UserRepository interface {
 	Create(*model.User) error
+	Find(int) (*model.User, error)
 	FindByEmail(string) (*model.User, error)
 }
